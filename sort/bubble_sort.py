2470b744706741f9e121e32944a6426ecac0f1b0
caoxiang104 <caoxiang112@gmail.com>
2018-06-06 22:03:43 +0800

    upgrade

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -1,7 +1,7 @@
 # o(n**2)
 def bubble_sort(array):
     for i in range(len(array) - 1):
-        for j in range(i, len(array)):
+        for j in range(i + 1, len(array)):
             if array[i] > array[j]:
                 array[i], array[j] = array[j], array[i]
     return array
